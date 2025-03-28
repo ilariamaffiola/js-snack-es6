c6af7bcbabe29ac089d8abebd3983bc7ea0b4df2
ilariamaffiola <maffiolailaria1@gmail.com>
2025-03-28 16:04:46 +0100

    [UPD]Add array of bicycle objects with name and weight properties

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//creare un array di oggetti 
+const bicycles = [
+    //ogni ogni oggetto decriverà una bici da corsa 
+    {
+        //ogni bici deve avere due proprietà : nome e peso
+        name: 'Trek Emonda SLR 9',
+        weight: 8,
+    } ,
+    {
+        name: 'Pinarello Dogma F12',
+        weight: 7,
+    } ,
+    {
+        name: 'Specialized S-Works Tarmac SL7',
+        weight: 6,
+    } ,
+    {
+        name: 'Cervélo R5',
+        weight: 7,
+    } ,
+    {
+        name: 'BMC Teammachine SLR01',
+        weight: 7,
+    }
+];
+console.log(bicycles);
